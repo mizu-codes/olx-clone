5f7c2e74b895bb51ef486958d36a2b3a5f15312a
mizu-codes <mizhanmichu9966@gmail.com>
2026-09-21 13:53:31 +0530

    Setup React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
+import AppRoutes from './routes/AppRoutes'
+
 function App() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        OLX Clone
-      </h1>
-    </div>
-  )
+  return <AppRoutes />
 }
 
 export default App
---
+++ src/pages/SellProduct/SellProduct.tsx
@@ -0,0 +1,5 @@
+function SellProduct() {
+  return <h1>Sell Product Page</h1>
+}
+
+export default SellProduct
